f3b1d04dba73cd2edfc6bf4e746dade1b081566b
Huy Phạm <huypd303@gmail.com>
2023-10-24 20:54:33 +0700

    bt1 updated

---
+++ src/OOP/BT2/Person/Information.java
@@ -0,0 +1,13 @@
+package OOP.BT2.Person;
+
+public class Information {
+    public static void main(String[] args) {
+        Person person = new Person();
+
+        System.out.println(person.getName());
+        System.out.println(person.getAddress());
+        System.out.println(person.getAge());
+        System.out.println(person.getGender());
+        System.out.println(person.getPhone());
+    }
+}
---
+++ src/OOP/BT2/Person/Person.java
@@ -0,0 +1,63 @@
+package OOP.BT2.Person;
+
+public class Person {
+    String name = "Huy";
+    int age = 20;
+    String gender = "nam";
+    String address = "Hà Nội";
+    String phone = "0123546789";
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
